9ac987e5367dcc878c6e600569934ee6589ada91
artur <artur@kramsoft.pl>
2019-05-04 16:29:15 +0200

    Create form types

---
+++ src/Form/ReservationType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+
+use App\Entity\Reservation;
+use App\Entity\Room;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ReservationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('person', PersonType::class)
+            ->add('checkIn', DateType::class, [
+                'format' => 'dd-MM-yy',
+            ])
+            ->add('checkOut', DateType::class, [
+                'format' => 'dd-MM-yy',
+            ])
+            ->add('room', EntityType::class, [
+                'class' => Room::class,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('u')
+                        ->orderBy('u.number', 'ASC');
+                },
+                'choice_label' => function ($room) { return $room; },
+            ])
+            ->add('save', SubmitType::class, ['label' => 'Add reservation']);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Reservation::class,
+        ]);
+    }
+
+}
